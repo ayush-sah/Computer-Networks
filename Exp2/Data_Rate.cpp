5da1cd8b56289f2f44b735eccb5e3c634a448cf4
ayush-sah <58354732+ayush-sah@users.noreply.github.com>
2020-02-01 21:43:58 +0530

    Update Data_Rate.cpp

--- Exp2/Data_Rate.cpp
+++ Exp2/Data_Rate.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 class DataRate{
     private:
-        int Bandwidth, value, Levels;
-        float Max_Capacity;
+        int Bandwidth, value, SNR, check, Levels;
+        float Capacity;
         string Noise;
     public:
         DataRate(){
             cout << "Enter the Bandwidth: ";
             cin >> Bandwidth;
-            Levels = 0;
             
             cout << "Enter Levels or SNR: ";
-            cin >> Levels;
+            cin >> value;
 
-            if(Levels!=0){
-                Max_Capacity = ((2 * Bandwidth) * (log(Levels)/log(2)));
-                cout << "Bit Rate = " << Max_Capacity << " bps." << endl;
+            check = value;
+            while(check>2){check/=2;}
+            
+            if(check%2==0){
+                Capacity = ((2 * Bandwidth) * (log(value)/log(2)));
+                cout << "Bit Rate = " << Capacity << " bps." << endl;
             }
             else{
-                cout << "Enter Signal to Noise ratio (SNR): ";
-                cin >> SNR;
-                Max_Capacity = (Bandwidth * (log(1+SNR)/log(2)));
-                cout << "Capacity = " << Max_Capacity << " bps." << endl;
-                Levels = pow(2,Max_Capacity/(2*Bandwidth));
+                Capacity = (Bandwidth * (log(1+value)/log(2)));
+                cout << "Capacity = " << Capacity << " bps." << endl;
+                Levels = pow(2, Capacity/(2*Bandwidth));
                 cout << "Levels = " << Levels << " bps." << endl;
             }
         }
