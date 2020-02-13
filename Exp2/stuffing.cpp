75f4347247e25b729056ec0ba81fd3dce92773c7
ayush-sah <ayush.sah@spit.ac.in>
2020-02-13 21:15:26 +0530

    Update stuffing.cpp

--- Exp2/stuffing.cpp
+++ Exp2/stuffing.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<bits/stdc++.h>
+#include<math.h>
 using namespace std;
 string str;
 class Stuffing{
@@ -22,7 +23,7 @@ public:
     void bitstuff(){
         string binary = "";
         string temp_bin = "";
-        for(int i = 0; i < str.length(); i++){
+        for(int i = 0; i <= str.length(); i++){
             asc = str[i];
             reverse(temp_bin.begin(),temp_bin.end());
             binary += temp_bin;
@@ -35,6 +36,7 @@ public:
                 asc/=2;
             }
         }
+        cout << str;
         cout << "\nEntered String in binary: " << binary << endl;
         int ones = 0;
         for(int i = 0; i < binary.length(); i++){
@@ -59,7 +61,20 @@ public:
                 binary.erase(binary.begin()+i+1);
             }
         }
-        cout << "\nDe-Stuffed Data \t: " << binary << endl << endl << endl;
+        cout << "\nDe-Stuffed Data \t: " << binary << endl;
+        int count = 7, decimal = 0;
+        string re_str = "";
+        for(int i = 0; i < binary.length(); i++){
+            count--;
+            if(binary.at(i)=='1')
+                decimal += pow(2.0,(float)count);
+            if(count==0){
+                re_str += (char)decimal;
+                decimal = 0;
+                count = 7;
+            }
+        }
+        cout << "\nRe-String \t: " << re_str << endl << endl << endl;
     }
 };
 
@@ -69,7 +84,7 @@ int main(){
     do{
         cout << "1. Character Stuffing\n2. Bit Stuffing\n3. Exit\n\nEnter action you want to perform: ";
         cin >> action;
-        if(action!=3){
+        if(action==1 || action==2){
             cout << "\nEnter string you want to stuff: ";
             cin.ignore();
             getline(cin,str);
