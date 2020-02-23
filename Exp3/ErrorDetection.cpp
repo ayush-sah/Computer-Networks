0fe181679d080e618da2a5d9c99d4339b7239176
ayush-sah <ayush.sah@spit.ac.in>
2020-02-23 23:36:06 +0530

    Update ErrorDetection.cpp

--- Exp3/ErrorDetection.cpp
+++ Exp3/ErrorDetection.cpp
@@ -8,13 +8,13 @@ class ErrorDetection{
     public:
     ErrorDetection(){
         do{
-            cout << "\n1. LRC\n2. VRC\n3. CRC\n4. Checksum\n5. Exit\nEnter action you want to perform: ";
+            cout << "\n1. LRC\n2. VRC\n3. Checksum\n4. CRC\n5. Exit\nEnter action you want to perform: ";
             cin >> action;
             switch(action){
                 case 1: lrc(); break;
                 case 2: vrc(); break;
-                case 3: crc(); break;
-                case 4: checksum(); break;
+                case 3: checksum(); break;
+                case 4: crc(); break;
                 case 5: break;
                 default: cout << "Invalid input. Enter Again.";
             }
@@ -130,55 +130,27 @@ class ErrorDetection{
     }
     
     void crc(){
-        string dividend, divisor;
+        string dividend, divisor, ans = "";
         cout << "Enter Dividend: ";
         cin >> dividend;
         
         cout << "Enter Divisor: ";
         cin >> divisor;
 
-        for(int i = 0; i < divisor.length()-1;i++)
-            dividend += '0';
+        ans += dividend;
+        for(int i = 1; i < divisor.length(); i++)
+            ans += '0';
         
-        int int_dividend[100], int_divisor[20];
-        for(int i = 0; i < dividend.length(); i++)
-            int_dividend[i] = (int)str[i];
-        
-        for(int i = 0; i < divisor.length(); i++)
-            int_divisor[i] = (int)str[i];
-
-        int temp[100];
-        int n;
-        for(int i = 0; i < sizeof(int_divisor)/sizeof(int_divisor[0]); i++){
-            temp[i] = int_dividend[i] ^ int_divisor[i];
-            n = i;
+        for(int i = 0; i <= ans.length()-divisor.length();){
+            for(int j = 0; j < divisor.length(); j++)
+                ans[i+j] = ans[i+j] == divisor[j]? '0' : '1';
+            while(i<ans.length()&& ans[i]!='1')
+                i++;
         }
-        int flag = 0, m = 0;
-        while(n <= sizeof(int_dividend)/sizeof(int_dividend[0])){
-            while(flag == 0){
-                if(res[m] == 0)
-                    m++;
-                else
-                    flag = 1;
-            }
-            flag = 0;
-            
-        }
-        /*
-        int dec_dividend = 0, dec_divisor = 0, power = 0;
-        for(int i = dividend.length()-1; i >= 0 ; i--){
-            if(dividend[i]=='1')
-                dec_dividend += pow(2.0, (float)power);
-            power++;
-        }
-        power = 0;
-        for(int i = divisor.length()-1; i >= 0 ; i--){
-            if(divisor[i]=='1')
-                dec_divisor += pow(2.0, (float)power);
-            power++;
-        }
-        int remainder = dec_dividend % dec_divisor;
-        */
+        int a = 0;
+        while(ans[a]!='1')
+            ans.erase(ans.begin()+0);
+        cout << "CRC is : " << dividend + ans << endl;
     }
 };
 
