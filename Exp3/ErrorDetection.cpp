66bef788b84083451c97f384ffc4572ff19dc8db
ayush-sah <ayush.sah@spit.ac.in>
2020-02-23 22:17:49 +0530

    Update ErrorDetection.cpp

--- Exp3/ErrorDetection.cpp
+++ Exp3/ErrorDetection.cpp
@@ -8,7 +8,7 @@ class ErrorDetection{
     public:
     ErrorDetection(){
         do{
-            cout << "\n1. LRC\n2. VRC\n3. CRC\n4. Checksum\n5. Exit\n6. Change String\nEnter action you want to perform on the string: ";
+            cout << "\n1. LRC\n2. VRC\n3. CRC\n4. Checksum\n5. Exit\nEnter action you want to perform: ";
             cin >> action;
             switch(action){
                 case 1: lrc(); break;
@@ -16,7 +16,6 @@ class ErrorDetection{
                 case 3: crc(); break;
                 case 4: checksum(); break;
                 case 5: break;
-                case 6: getstring(); break;
                 default: cout << "Invalid input. Enter Again.";
             }
         }while(action!=5);
@@ -26,9 +25,11 @@ class ErrorDetection{
         cout << "Enter the string: ";
         num = 0;
         char c;
+        str[0] = "";
         while (cin.peek()!='\n'){
             if(cin.peek()==' '){
                 num++;
+                str[num] = "";
             }
             cin >> c;
             str[num] += c;
@@ -37,6 +38,7 @@ class ErrorDetection{
     }
 
     void lrc(){
+        cin.ignore();
         getstring();
         ans = "";
         for(int i = 0; i <= num; i++){
@@ -54,6 +56,7 @@ class ErrorDetection{
     }
 
     void vrc(){
+        cin.ignore();
         getstring();
         ans = "";
         for(int i = 0; i < len; i++){
@@ -71,6 +74,7 @@ class ErrorDetection{
     }
 
     void checksum(){
+        cin.ignore();
         getstring();
         ans = "";
         string temp = "";
@@ -129,10 +133,38 @@ class ErrorDetection{
         string dividend, divisor;
         cout << "Enter Dividend: ";
         cin >> dividend;
+        
         cout << "Enter Divisor: ";
         cin >> divisor;
+
         for(int i = 0; i < divisor.length()-1;i++)
             dividend += '0';
+        
+        int int_dividend[100], int_divisor[20];
+        for(int i = 0; i < dividend.length(); i++)
+            int_dividend[i] = (int)str[i];
+        
+        for(int i = 0; i < divisor.length(); i++)
+            int_divisor[i] = (int)str[i];
+
+        int temp[100];
+        int n;
+        for(int i = 0; i < sizeof(int_divisor)/sizeof(int_divisor[0]); i++){
+            temp[i] = int_dividend[i] ^ int_divisor[i];
+            n = i;
+        }
+        int flag = 0, m = 0;
+        while(n <= sizeof(int_dividend)/sizeof(int_dividend[0])){
+            while(flag == 0){
+                if(res[m] == 0)
+                    m++;
+                else
+                    flag = 1;
+            }
+            flag = 0;
+            
+        }
+        /*
         int dec_dividend = 0, dec_divisor = 0, power = 0;
         for(int i = dividend.length()-1; i >= 0 ; i--){
             if(dividend[i]=='1')
@@ -145,7 +177,8 @@ class ErrorDetection{
                 dec_divisor += pow(2.0, (float)power);
             power++;
         }
-        int remainder = dec_dividend / dec_divisor;
+        int remainder = dec_dividend % dec_divisor;
+        */
     }
 };
 
